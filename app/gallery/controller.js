54027f8f7e8317280598adc781370dc5e992b70f
Fauzan Husain <achmadfauzan1340@gmail.com>
2023-04-17 14:43:59 +0800

    update gallery

--- app/gallery/controller.js
+++ app/gallery/controller.js
@@ -1,5 +1,9 @@
 const Gallery = require("./model")
 
+const path = require("path");
+const fs = require("fs")
+const config = require("../../config")
+
 module.exports = {
     index: async(req, res) => {
         try {
@@ -20,6 +24,127 @@ module.exports = {
         } catch (err) {
             res.redirect("/gallery")
             throw err
+        }
+    },
+    actionCreate: async(req, res) => {
+        try {
+            const { title } = req.body;
+
+            if(req.file){
+                let tmp_path = req.file.path;
+                let originalExt = req.file.originalname.split(".")[req.file.originalname.split(".").length - 1];
+                let filename = req.file.filename + "." + originalExt;
+                let target_path = path.resolve(config.rootPath, `public/uploads/gallery/${filename}`);
+
+                const src = fs.createReadStream(tmp_path);
+                const dest = fs.createWriteStream(target_path);
+
+                src.pipe(dest);
+
+                src.on("end", async() => {
+                    try {
+                        const gallery = new Gallery({
+                            title,
+                            image: filename
+                        })
+
+                        await gallery.save();
+            
+                        res.redirect("/gallery");
+                    } catch (err) {
+                        res.redirect("/gallery");
+                    }
+                })
+            } else {
+                res.redirect("/gallery");
+            }
+        } catch (err) {
+            res.redirect("/gallery")
+            throw err
+        }
+    },
+    viewEdit: async(req, res) => {
+        try {
+            const { id } = req.params;
+            const gallery = await Gallery.findOne({ _id: id })
+
+            res.render("admin/gallery/edit", {
+                title: "Fight Camp || Edit gallery",
+                gallery
+            })
+        } catch (err) {
+            res.redirect("/gallery")
+            throw err
+        }
+    },
+    actionEdit: async(req, res) => {
+        try {
+            const { id } = req.params;
+            const { title } = req.body;
+
+            if(req.file){
+                let tmp_path = req.file.path;
+                let originalExt = req.file.originalname.split(".")[req.file.originalname.split(".").length - 1];
+                let filename = req.file.filename + "." + originalExt;
+                let target_path = path.resolve(config.rootPath, `public/uploads/gallery/${filename}`);
+
+                const src = fs.createReadStream(tmp_path);
+                const dest = fs.createWriteStream(target_path);
+
+                src.pipe(dest);
+
+                src.on("end", async() => {
+                    try {
+                        const gallery = await Gallery.findOne({ _id: id });
+
+                        let currentImage = `${config.rootPath}/public/uploads/gallery/${gallery.image}`;
+                        if(fs.existsSync(currentImage)) {
+                            fs.unlinkSync(currentImage)
+                        }
+
+                        await Gallery.findOneAndUpdate({
+                            _id: id
+                        },{
+                            title,
+                            image: filename
+                        })
+
+                        await gallery.save();
+
+                        res.redirect("/gallery");
+                    } catch (err) {
+                        res.redirect("/gallery");
+                    }
+                })
+            } else {
+                await Gallery.findOneAndUpdate({
+                    _id: id
+                },{
+                    title,
+                })
+
+                res.redirect("/gallery");
+            }
+        } catch (err) {
+            res.redirect("/gallery");
+        }
+    },
+    actionDelete: async(req, res) => {
+        try {
+            const { id } = req.params;
+
+            const gallery = await Gallery.findOneAndRemove({
+                _id: id
+            });
+
+            let currentImage = `${config.rootPath}/public/uploads/gallery/${gallery.image}`;
+            if(fs.existsSync(currentImage)){
+                fs.unlinkSync(currentImage)
+            }
+            
+            res.redirect("/gallery");
+        } catch (err) {
+            res.redirect("/gallery");
         }
     }
 }
--- app/gallery/router.js
+++ app/gallery/router.js
@@ -1,10 +1,18 @@
 var express = require('express');
 var router = express.Router();
-const { index, viewCreate } = require("./controller")
+const multer = require("multer")
+
+const { index, viewCreate, actionCreate, viewEdit, actionEdit, actionDelete } = require("./controller")
+
+const upload = multer({ dest: '/public/uploads/logo' })
 const { isLoginAdmin } = require("../middleware/auth")
 
 router.use(isLoginAdmin)
 router.get('/', index);
 router.get('/add', viewCreate);
+router.post('/add', upload.single("image"), actionCreate);
+router.get('/edit/:id', viewEdit);
+router.put('/edit/:id', upload.single("image"), actionEdit);
+router.delete('/delete/:id', actionDelete)
 
 module.exports = router;
