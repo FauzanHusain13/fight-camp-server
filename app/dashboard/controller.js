a5849a7877891c62e93634fbb58342d15d0dd50f
Fauzan Husain <achmadfauzan1340@gmail.com>
2023-04-22 19:00:59 +0800

    update dashboard

--- app/dashboard/controller.js
+++ app/dashboard/controller.js
@@ -1,8 +1,24 @@
+const Membership = require("../membership/model")
+const Bank = require("../bank/model")
+const Training = require("../training/model")
+const Confirmation = require("../confirmation/model")
+
 module.exports = {
     index: async(req, res) => {
         try {
+            const membership = await Membership.countDocuments();
+            const bank = await Bank.countDocuments();
+            const training = await Training.countDocuments();
+            const confirmation = await Confirmation.countDocuments();
+
             res.render("admin/dashboard/view_dashboard", {
                 title: "Fight Camp || Dashboard",
+                count: {
+                    membership,
+                    bank,
+                    training,
+                    confirmation
+                }
             })
         } catch (err) {
             throw err
